c464045cc8aaaea1d4613c4c909857c16c3932be
ispedals <supronitbat@xynl.at>
2013-05-29 14:55:36 -0400

    Add launcher activity FloatingJapaneseDictionaryLauncherActivity

---
+++ src/pedals/is/floatingjapanesedictionary/FloatingJapaneseDictionaryLauncherActivity.java
@@ -0,0 +1,13 @@
+package pedals.is.floatingjapanesedictionary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FloatingJapaneseDictionaryLauncherActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		finish();
+	}
+}
