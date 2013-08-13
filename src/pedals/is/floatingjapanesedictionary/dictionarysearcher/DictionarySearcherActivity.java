a4b09619934e7f01f588cef028cd8d0ed7681c11
ispedals <supronitbat@xynl.at>
2013-08-13 18:55:17 -0400

    remove unnecessary comment in DictionarySearcherActivity

--- src/pedals/is/floatingjapanesedictionary/dictionarysearcher/DictionarySearcherActivity.java
+++ src/pedals/is/floatingjapanesedictionary/dictionarysearcher/DictionarySearcherActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 
 public class DictionarySearcherActivity extends Activity {
 
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 
