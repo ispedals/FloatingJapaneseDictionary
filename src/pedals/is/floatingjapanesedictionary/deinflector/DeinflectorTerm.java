5aeabc4e368b77356d361ef44444a83d58f06c43
ispedals <supronitbat@xynl.at>
2013-08-13 19:04:25 -0400

    Add license and notices

--- src/pedals/is/floatingjapanesedictionary/deinflector/DeinflectorTerm.java
+++ src/pedals/is/floatingjapanesedictionary/deinflector/DeinflectorTerm.java
@@ -1,3 +1,36 @@
+/*
+ *  Copyright 2013 Balloonguy
+ *
+ *  Based on Rikaichan 2.0.7
+ *
+	---
+
+	Rikaichan
+	Copyright (C) 2005-2012 Jonathan Zarate
+	http://www.polarcloud.com/
+
+	---
+
+	Originally based on RikaiXUL 0.4 by Todd Rudick
+	http://www.rikai.com/
+	http://rikaixul.mozdev.org/
+
+	---
+ *  This file is part of FloatingJapaneseDictionary.
+
+    FloatingJapaneseDictionary is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    FloatingJapaneseDictionary is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FloatingJapaneseDictionary.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package pedals.is.floatingjapanesedictionary.deinflector;
 
 public class DeinflectorTerm {
--- src/pedals/is/floatingjapanesedictionary/dictionarysearcher/DictionaryEntry.java
+++ src/pedals/is/floatingjapanesedictionary/dictionarysearcher/DictionaryEntry.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright 2013 Balloonguy
+ *  This file is part of FloatingJapaneseDictionary.
+
+    FloatingJapaneseDictionary is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    FloatingJapaneseDictionary is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FloatingJapaneseDictionary.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package pedals.is.floatingjapanesedictionary.dictionarysearcher;
 
 import android.content.ContentValues;
